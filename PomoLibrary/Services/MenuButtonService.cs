45ce17192a75a4745bbc209278f25ac4caba91b0
Colin Kiama <colinkiama@gmail.com>
2019-12-19 22:40:23 +0000

    MenuButton's glyph now binds to property in MenuButtonService

--- PomoLibrary/Services/MenuButtonService.cs
+++ PomoLibrary/Services/MenuButtonService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PomoLibrary.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,21 +7,49 @@ using System.Threading.Tasks;
 
 namespace PomoLibrary.Services
 {
-    public class MenuButtonService
+    public class MenuButtonService : Notifier
     {
+        const string MenuGlyphString = "\xE700";
+        const string BackGlyphString = "\xE830";
+
         // Singleton Pattern with "Lazy"
         private static Lazy<MenuButtonService> lazy =
             new Lazy<MenuButtonService>(() => new MenuButtonService());
 
         public static MenuButtonService Instance => lazy.Value;
 
-        private MenuButtonService() { }
+        private string _currentButtonGlyph;
+
+        public string CurrentButtonGlyph
+        {
+            get { return _currentButtonGlyph; }
+            set
+            {
+                _currentButtonGlyph = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private MenuButtonService()
+        {
+            CurrentButtonGlyph = MenuGlyphString;
+        }
 
         public event EventHandler MenuClosed;
 
         public void CloseMenu()
         {
             MenuClosed?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void NavigatedToMenu()
+        {
+            CurrentButtonGlyph = MenuGlyphString;
+        }
+
+        public void NavigatedToMenuChild()
+        {
+            CurrentButtonGlyph = BackGlyphString;
         }
     }
 }
